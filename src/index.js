46971bc496de00590de5dd4ffa7f19b2f68a5cae
adypr <48469764+adypr@users.noreply.github.com>
2019-10-03 23:53:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,41 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const ten = (str) => {
+        const brakeArr = (str, arr) => {
+            if (str.length === 10) {
+                arr.push(str);
+                return arr;
+            }
+            arr.push(str.slice(0, 10));
+            return brakeArr(str.slice(10), arr);
+        }
+        return brakeArr(str, []);
+    }
+
+    const noZero = str => {
+        if (str[0] !== '0') {
+            return str;
+        }
+        return noZero(str.slice(1));
+    }
+    
+    const pairs = str => {
+        const letter = (str, resStr) => {
+            if (str.length === 2) {
+                return str === '10' ? resStr + '.' : resStr + '-';
+            }
+            let res = str.slice(0, 2) === '10' ? resStr + '.' : resStr + '-';
+
+            return letter(str.slice(2), res);
+        }
+        return letter(str, '');
+    }
+
+    return expr.split('**********').map(str => ten(str).map(str2 => noZero(str2)).map(str3 => pairs(str3)).map(str4 => MORSE_TABLE[str4]).join('')).join(' ');
+
+}
+
 }
 
 module.exports = {
